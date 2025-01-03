cf7c9745386bbab78e685c281abaa4c4573146b4
Mohd faiz <92942111+mfaiz61926@users.noreply.github.com>
2025-01-03 23:17:14 +0530

    Add files via upload

---
+++ day16.py
@@ -0,0 +1,12 @@
+x=int(input("enter the value of x:"))
+match x:
+    case 0:
+        print("x is zero")
+    case 4:
+        print("case is 4")
+    case _ if x!=90:
+        print(x,"is not 98")
+    case _ if x!=80:
+        print(x,"x is not 80 ")
+    case _:
+        print(x)
